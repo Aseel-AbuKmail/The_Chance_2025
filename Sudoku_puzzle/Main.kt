dfab285533de81c61b962e203877594c109052b9
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-04-04 15:41:09 +0200

    Update Main.kt

--- Sudoku_puzzle/Main.kt
+++ Sudoku_puzzle/Main.kt
@@ -1,1 +1,26 @@
+fun main() {
+    val board = arrayOf(
+        intArrayOf(5, 3, 0, 0, 7, 0, 0, 0, 0),
+        intArrayOf(6, 0, 0, 1, 9, 5, 0, 0, 0),
+        intArrayOf(0, 9, 8, 0, 0, 0, 0, 6, 0),
+        intArrayOf(8, 0, 0, 0, 6, 0, 0, 0, 3),
+        intArrayOf(4, 0, 0, 8, 0, 3, 0, 0, 1),
+        intArrayOf(7, 0, 0, 0, 2, 0, 0, 0, 6),
+        intArrayOf(0, 6, 0, 0, 0, 0, 2, 8, 0),
+        intArrayOf(0, 0, 0, 4, 1, 9, 0, 0, 5),
+        intArrayOf(0, 0, 0, 0, 8, 0, 0, 7, 9)
+    )
 
+    if(SudokuSolver.solve(board)) {
+        println("Solved Sudoku:")
+        printBoard(board)
+    } else {
+        println("No solution exists.")
+    }
+}
+
+fun printBoard(board: Array<IntArray>) {
+    for (row in board) {
+        println(row.joinToString(" ") { it.toString() })
+    }
+}
